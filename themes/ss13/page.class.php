07bdf4f3d91a32407e1785e0b28948d35fa4628c
gurfan <51427771+gurfan@users.noreply.github.com>
2022-11-19 20:54:39 -0600

    fix dat

--- themes/ss13/page.class.php
+++ themes/ss13/page.class.php
@@ -30,7 +30,7 @@ class Page extends BasePage
 
         $menu = "<div class='menu'>
 			<script type='text/javascript' src='{$data_href}/themes/{$theme_name}/wz_tooltip.js'></script>
-			<a href='/' class='menu-logo'><img alt='' src='{$data_href}/favicon.ico' style='position: relative; top: 3px;'>
+			<a href='/' class='menu-logo'><img alt='' src='{$data_href}/themes/{$theme_name}/clown_64.png' style='position: relative; top: 3px;'>
 			<b>{$site_name}</b></a> ";
 
         // Custom links: These appear on the menu.
