29102d76e8be6817b697dc2ca51bf2840430fb9b
gurfan <51427771+gurfan@users.noreply.github.com>
2023-01-13 17:43:19 -0600

    Update main.php

--- ext/setup/main.php
+++ ext/setup/main.php
@@ -359,7 +359,8 @@ class Setup extends Extension
 				var http_request = new XMLHttpRequest();
 				http_request.open('GET', '$test_url', false);
 				http_request.send(null);
-
+                console.log(http_request.responseText);
+                console.log(http_request.status);
 				if(http_request.status === 200 && http_request.responseText === 'ok') {
 					checkbox.disabled = false;
 					out_span.innerHTML = '(tested ok)';
