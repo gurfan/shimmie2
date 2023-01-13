24a2774ed864ddec08469e7c9e5a55927dbebf16
gurfan <51427771+gurfan@users.noreply.github.com>
2023-01-13 15:22:59 -0600

    filter explicit posts from random image block

--- ext/random_image/main.php
+++ ext/random_image/main.php
@@ -53,7 +53,7 @@ class RandomImage extends Extension
     {
         global $config, $page;
         if ($config->get_bool("show_random_block")) {
-            $image = Image::by_random($event->search_terms);
+            $image = Image::by_random(array_push($event->search_terms, "-rating:explicit"));
             if (!is_null($image)) {
                 $this->theme->display_random($page, $image);
             }
