0f30a66c26790dd5557add0d6236160a24d14ab0
gurfan <51427771+gurfan@users.noreply.github.com>
2023-02-04 14:31:14 -0600

    metatag

--- themes/ss13/home.theme.php
+++ themes/ss13/home.theme.php
@@ -19,6 +19,7 @@ class CustomHomeTheme extends Themelet
 		<title>$sitename</title>
 		<meta http-equiv="Content-Type" content="text/html;charset=utf-8">
 		<meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="An image booru for all Space Station 13 content!">
 		$hh
 	</head>
 	<body>
