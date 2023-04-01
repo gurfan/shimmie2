76e3e39712c43252ad9585f9cf6f4e6767ceaf1b
gurfan <51427771+gurfan@users.noreply.github.com>
2023-04-01 14:27:15 -0500

    screenshots

---
+++ ext/screenshots/info.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class ScreenshotsTabInfo extends ExtensionInfo
+{
+    public const KEY = "screenshots_tab";
+
+    public string $key = self::KEY;
+    public string $name = "Screenshots Tab";
+    public string $url = "";
+    public array $authors = "";
+    public string $license = self::LICENSE_GPLV2;
+    public string $description = "Isolates game screenshots to a seperate tab.";
+    public ?string $documentation = "";
+}
---
+++ ext/screenshots/theme.php
@@ -0,0 +1,58 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Shimmie2;
+
+class ScreenshotListTheme extends Themelet
+{
+    protected array $search_terms;
+
+    /**
+     * #param string[] $search_terms
+     */
+    public function set_page(array $search_terms)
+    {
+        $this->search_terms = $search_terms;
+    }
+
+    /**
+     * #param Image[] $images
+     */
+    public function display_page(Page $page, array $images)
+    {
+        $page->title = "Screenshots";
+
+        if (count($images)) {
+            $html .= "<div class='shm-image-list'>";
+
+            foreach ($images as $image) {
+                $html .= $this->build_thumb_html($image);
+            }
+
+            $html .= "</div>";
+        } else {
+            $html .= "<br/><br/>No posts were found to match the search criteria";
+        }
+
+        $page->add_block(new Block("Screenshots", $html));
+
+        $nav = $this->build_navigation($this->search_terms);
+        $page->add_block(new Block("Navigation", $nav, "left", 0));
+    }
+
+    protected function build_navigation(array $search_terms): string
+    {
+        $h_search_string = html_escape(Tag::implode($search_terms));
+        $h_search_link = make_link("screenshots");
+        $h_search = "
+			<p><form action='$h_search_link' method='GET'>
+				<input type='search' name='search' value='$h_search_string' placeholder='Search screenshot list' class='autocomplete_tags' autocomplete='off' />
+				<input type='hidden' name='q' value='/screenshots'>
+				<input type='submit' value='Find' style='display: none;' />
+			</form>
+		";
+
+        return $h_search;
+    }
+}
