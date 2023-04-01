e0474a9e7768aa369ab918bfb488bdd2a2f82251
gurfan <51427771+gurfan@users.noreply.github.com>
2023-04-01 14:53:48 -0500

    Update info.php

--- ext/screenshots/info.php
+++ ext/screenshots/info.php
@@ -4,7 +4,7 @@ declare(strict_types=1);
 
 namespace Shimmie2;
 
-class ScreenshotsListInfo extends ExtensionInfo
+class ScreenshotListInfo extends ExtensionInfo
 {
     public const KEY = "screenshots_list";
 
