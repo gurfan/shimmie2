5ddda52e94c09aae2f3850d8bd93023a511748ab
gurfan <51427771+gurfan@users.noreply.github.com>
2023-04-01 14:56:36 -0500

    fuck

--- ext/screenshots/info.php
+++ ext/screenshots/info.php
@@ -9,10 +9,9 @@ class ScreenshotListInfo extends ExtensionInfo
     public const KEY = "screenshots_list";
 
     public string $key = self::KEY;
-    public string $name = "Screenshots List";
+    public string $name = "Screenshot List";
     public string $url = "";
     public array $authors = ["BooruAdmin"];
     public string $license = self::LICENSE_GPLV2;
     public string $description = "Isolates game screenshots to a seperate tab.";
-    public ?string $documentation = "";
 }
