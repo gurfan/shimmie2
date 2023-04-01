5d58b3619df0d97c02a5f95f6e441ec58ba3ab04
gurfan <51427771+gurfan@users.noreply.github.com>
2023-04-01 14:59:13 -0500

    fuck it

--- ext/random_list/main.php
+++ ext/random_list/main.php
@@ -51,6 +51,55 @@ class RandomList extends Extension
             $this->theme->set_page($search_terms);
             $this->theme->display_page($page, $random_images);
         }
+
+        if ($event->page_matches("screenshots")) {
+            if (isset($_GET['search'])) {
+                // implode(explode()) to resolve aliases and sanitise
+                $search = url_escape(Tag::implode(Tag::explode($_GET['search'], false)));
+                if (empty($search)) {
+                    $page->set_mode(PageMode::REDIRECT);
+                    $page->set_redirect(make_link("screenshots"));
+                } else {
+                    $page->set_mode(PageMode::REDIRECT);
+                    $page->set_redirect(make_link('screenshots/'.$search));
+                }
+                return;
+            }
+
+            if ($event->count_args() == 0) {
+                $search_terms = [];
+            } elseif ($event->count_args() == 1) {
+                $search_terms = explode(' ', $event->get_arg(0));
+            } else {
+                throw new SCoreException("Error: too many arguments.");
+            }
+
+            array_push($search_terms, "screenshot");
+
+            $page_number = $event->get_page_number();
+            $page_size = $event->get_page_size();
+
+            $total_pages = Image::count_pages($search_terms);
+            $images = Image::find_images(($page_number-1)*$page_size, $page_size, $search_terms);
+
+            $count_images = count($images);
+            $count_search_terms = count($search_terms);
+
+
+            if ($count_images === 1 && $page_number === 1) {
+                $page->set_mode(PageMode::REDIRECT);
+                $page->set_redirect(make_link('post/view/'.$images[0]->id));
+            } else {
+                $plbe = new PostListBuildingEvent($search_terms);
+                send_event($plbe);
+
+                $this->theme->set_page($page_number, $total_pages, $search_terms);
+                $this->theme->display_page($page, $images);
+                if (count($plbe->parts) > 0) {
+                    $this->theme->display_admin_block($plbe->parts);
+                }
+            }
+        }
     }
 
     public function onInitExt(InitExtEvent $event)
@@ -74,6 +123,7 @@ class RandomList extends Extension
     {
         if ($event->parent=="posts") {
             $event->add_nav_link("posts_random", new Link('random'), "Shuffle");
+            $event->add_nav_link("posts_screenshots", new Link('screenshots'), "Screenshots");
         }
     }
 }
